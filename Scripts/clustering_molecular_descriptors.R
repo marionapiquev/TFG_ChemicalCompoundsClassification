641311ff417e163e30e432f307b7c9d4d0df815d
marionapiquev <44998576+marionapiquev@users.noreply.github.com>
2023-04-12 17:39:09 +0200

    Add files via upload

---
+++ Scripts/clustering_molecular_descriptors.R
@@ -0,0 +1,120 @@
+########################################
+#INSTALLING R PACKAGES (ONLY RUN ONCE!!!)
+#if (!requireNamespace("BiocManager", quietly = TRUE))
+#install.packages("BiocManager")
+#BiocManager::install("ChemmineR")
+#BiocManager::install("Rcpi")
+#BiocManager::install("ChemmineOB")
+
+#library(devtools)
+#install_github("CDK-R/rpubchem", dependencies=TRUE)
+#devtools::install_github('rajarshi/chemblr/package')
+########################################
+#LOADING R PACKAGES
+library(rcdk)
+library(ChemmineR) 
+library(ChemmineOB) 
+library(rpubchem)
+library(chemblr)
+library(fingerprint)
+library(Rcpi)
+library(cluster)
+library("factoextra")
+library(fpc)
+library(ggcorrplot)
+library(plotly)
+library(ggplot2)
+library(dbscan)
+########################################
+#LOAD THE DATAFRAME
+#Set the directory 
+setwd("/Users/MarionaP/Desktop/TFG/DrugBank")
+DF <- read.csv("df_molecular_descriptors_scaled.csv")
+
+########################################
+#K-MEANS
+cnames <- colnames(DF)
+df <- DF[,cnames[!cnames %in% c("X","id")]]
+#S1. Choose the number of clusters using the elbow method
+#M1. Elbow Method
+fviz_nbclust(df, FUN = kmeans, k.max = 20, method = "gap_stat")
+
+km.res <- kmeans(pca_DF, 10)
+km.res
+fviz_cluster(km.res, data = pca_DF, ellipse.type = "convex")
+
+df_km <- pca_DF
+df_km['cluster'] <- km.res['cluster']
+
+
+fig <- plot_ly(df_km, x = ~Comp.1, y = ~Comp.2, z = ~Comp.3, color = ~cluster)
+fig <- fig %>% add_markers()
+fig <- fig %>% layout(scene = list(xaxis = list(title = 'MLogP'),
+                                   yaxis = list(title = 'OB_MW'),
+                                   zaxis = list(title = 'OB_MR')))
+
+fig
+
+pca.DF <- princomp(df) 
+summary(pca.DF)
+#Show the percentage of variances explained by each principal component.
+fviz_eig(pca.DF)
+#Graph of individuals (Individuals with a similar profile are grouped together)
+fviz_pca_ind(pca.DF,
+             col.ind = "cos2", 
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE     # Avoid text overlapping
+)
+pca_DF <- as.data.frame(pca.DF$scores)
+p <- plot_ly(pca_DF, x=pca_DF$Comp.1, y=pca_DF$Comp.2, 
+             z=pca_DF$Comp.3) %>%
+  add_markers(size=1.5)
+print(p)
+
+########################################
+#HIERARCHICAL CLUSTERING
+#Prepare the data for HC
+cnames <- colnames(DF)
+df <- DF[,cnames[!cnames %in% c("X","id","X.1")]]
+dist_matrix <- daisy(df, metric = "euclidean")
+
+#Perform the clustering
+hc <- hclust(dist_matrix, method = 'ward.D')
+plot(hc,main = paste('Dendrogram'), xlab = 'Molecules', ylab = 'Euclidean distances')
+
+#Cut tree in n clusters
+grp <- cutree(hc, k = 4)
+plot(hc,main = paste('Dendrogram'),xlab = 'Molecules', ylab = 'Euclidean distances') 
+rect.hclust(hc, k = 4, border = 2:5)
+
+#Add clustering results to a dataframe
+df_hc <- DF
+df_hc['cluster'] <- grp
+
+#Visualize the clustering
+fig <- plot_ly(df_hc, x = ~MLogP, y = ~OB_MW, z = ~OB_logP, color = ~cluster)
+fig <- fig %>% add_markers()
+fig <- fig %>% layout(scene = list(xaxis = list(title = 'MLogP'),
+                                   yaxis = list(title = 'OB_MW'),
+                                   zaxis = list(title = 'OB_logP')))
+fig
+
+
+ggplot(data = df_hc,
+       mapping = aes(x = MLogP, 
+                     y = OB_MW,
+                     color = cluster))+
+  geom_point() 
+
+########################################
+#DBSCAN
+dbscan_result <- dbscan(df, eps = 2, minPts = 10)
+df_dbscan <- DF
+df_dbscan['cluster'] <- dbscan_result$cluster
+
+df_clusters <- filter(df_dbscan, cluster > 0)
+ggplot(data = df_clusters,
+       mapping = aes(x = MLogP, 
+                     y = OB_MW,
+                     color = cluster))+
+  geom_point() 
