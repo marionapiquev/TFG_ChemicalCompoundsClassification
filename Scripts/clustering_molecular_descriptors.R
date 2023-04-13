ded8aaf7986286f20db8167d0636f885f3e92e2d
marionapiquev <44998576+marionapiquev@users.noreply.github.com>
2023-04-13 16:52:48 +0200

    DBSCAN clustering visualitzation

--- Scripts/clustering_molecular_descriptors.R
+++ Scripts/clustering_molecular_descriptors.R
@@ -30,7 +30,7 @@ library(dbscan)
 #Set the directory 
 setwd("/Users/MarionaP/Desktop/TFG/DrugBank")
 DF <- read.csv("df_molecular_descriptors_scaled.csv")
-
+smiles <- read.csv("df_smiles.csv")
 ########################################
 #K-MEANS
 #Prepare the data for clustering
@@ -99,10 +99,10 @@ ggplot(data = df_hc, mapping = aes(x = MLogP, y = OB_MW,color = cluster))+geom_p
 #DBSCAN
 #Prepare the data for clustering
 cnames <- colnames(DF)
-df <- DF[,cnames[!cnames %in% c("X","id")]]
+df <- DF[,cnames[!cnames %in% c("X","id","X.1")]]
 
 #Perform the clustering
-dbscan_result <- dbscan(df, eps = 2, minPts = 10)
+dbscan_result <- dbscan(df,eps = 2, minPts = 10)
 
 #Add clustering results to a dataframe
 df_dbscan <- DF
@@ -111,3 +111,18 @@ df_dbscan['cluster'] <- dbscan_result$cluster
 #Visualize the clustering
 df_clusters <- filter(df_dbscan, cluster > 0)
 ggplot(data = df_clusters, mapping = aes(x = MLogP,y = OB_MW, color = cluster))+geom_point() 
+
+#Visualize molecules 
+for (i in 1:4 ){
+  cluster1 <- filter(df_dbscan, cluster == i)
+  c1_id <- cluster1[cnames[cnames %in% c("id")]]
+  c1_smiles <- filter(smiles, id %in% as.list(c1_id)$id)
+  c1_sdf <- c()
+  for (i in as.list(c1_smiles['smile'])$smile){
+    c1_sdf <- c(c1_sdf, smiles2sdf(i))
+  }
+  par(mfrow=c(4,4))
+  for (i in 1:length(c1_sdf)){
+    openBabelPlot(c1_sdf[[i]])
+  }
+}
