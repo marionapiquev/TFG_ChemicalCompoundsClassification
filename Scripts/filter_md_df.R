b5b45644f9c40ae07dc98ed8586a05d7ab5af4e7
marionapiquev <44998576+marionapiquev@users.noreply.github.com>
2023-04-17 00:37:24 +0200

    Add files via upload

---
+++ Scripts/filter_md_df.R
@@ -0,0 +1,40 @@
+########################################
+#LOAD THE DATAFRAME
+#Set the directory 
+setwd("/Users/MarionaP/Desktop/TFG/DrugBank")
+df <- read.csv("df_molecular_descriptors.csv")
+########################################
+#FILTERING FUNCTION
+filter_func <- function(df, MW, Aromatic, MLogP) {
+  if (MW == 'S'){
+    df <- filter(df, OB_MW < 900)
+  }
+  else if (MW == 'B'){
+    df <- filter(df, OB_MW > 900)
+  }
+  
+  if (Aromatic == TRUE){
+    df <- filter(df, naAromAtom > 0)
+  }
+  else if (Aromatic == FALSE){
+    df <- filter(df, naAromAtom == 0)
+  }
+  
+  if (MLogP == 'P'){
+    df <- filter(df, MLogP > 0)
+  }
+  else if (MLogP == 'N'){
+    df <- filter(df, MLogP < 0)
+  }
+  
+  write.csv(df, "df_md_filtered.csv")
+  return(df)
+}
+
+
+df_f <- filter_func(df, MW = 'S', Aromatic = TRUE, MLogP = FALSE)
+
+#Filter function
+df_f1 <- filter(prop_df, OB_MW < 500  & Aromatic > 0 & MLogP > 0)
+
+
