26906a87ea8619367e7d58a73c25efd938cfee0a
marionapiquev <44998576+marionapiquev@users.noreply.github.com>
2023-04-25 10:08:12 +0200

    Add files via upload

--- Scripts/clustering_molecular_descriptors.R
+++ Scripts/clustering_molecular_descriptors.R
@@ -25,6 +25,7 @@ library(ggcorrplot)
 library(plotly)
 library(ggplot2)
 library(dbscan)
+library(matrixStats)
 ########################################
 #LOAD THE DATAFRAME
 #Set the directory 
@@ -57,15 +58,18 @@ print(p)
 ########################################
 #K-MEANS
 #Prepare the data for clustering
-DF <- df_filter
+DF <- Df_scaled #Choose the dataframe you want to use
 cnames <- colnames(DF)
 df <- DF[,cnames[!cnames %in% c("X","id")]]
+
+#Since we are with a high dimension dataframe we use PCA to make the clustering easier
+pca_DF <- pca(df)
 
 #Perform the clustering
 km.res <- kmeans(pca_DF, 10)
 km.res
 
-#Add clustering results to a dataframe
+#Add clustering results to a dataframe using PCA dataframe
 df_km <- pca_DF
 df_km['cluster'] <- km.res['cluster']
 
@@ -75,10 +79,20 @@ fig <- fig %>% add_markers()
 fig <- fig %>% layout(scene = list(xaxis = list(title = 'MLogP'),yaxis = list(title = 'OB_MW'),zaxis = list(title = 'OB_MR')))
 fig
 
+#Add clustering results to a dataframe using the original dataframe
+df_km <- df_original
+df_km['cluster'] <- km.res['cluster']
+
+#Visualize the clustering
+fig <- plot_ly(df_km, x = ~MLogP, y = ~OB_MW, z = ~OB_logP, color = ~cluster)
+fig <- fig %>% add_markers()
+fig <- fig %>% layout(scene = list(xaxis = list(title = 'MLogP'),yaxis = list(title = 'OB_MW'),zaxis = list(title = 'OB_logP')))
+fig
+
 ########################################
 #HIERARCHICAL CLUSTERING
 #Prepare the data for HC
-DF <- df_filter
+DF <- Df_scaled #Choose the dataframe you want to use
 cnames <- colnames(DF)
 df <- DF[,cnames[!cnames %in% c("X","id","X.1")]]
 dist_matrix <- daisy(df, metric = "euclidean")
@@ -93,7 +107,7 @@ plot(hc,main = paste('Dendrogram'),xlab = 'Molecules', ylab = 'Euclidean distanc
 rect.hclust(hc, k = 10, border = 2:5)
 
 #Add clustering results to a dataframe
-df_hc <- DF
+df_hc <- df_original
 df_hc['cluster'] <- grp
 
 #Visualize the clustering
@@ -102,6 +116,14 @@ fig <- fig %>% add_markers()
 fig <- fig %>% layout(scene = list(xaxis = list(title = 'MLogP'),yaxis = list(title = 'OB_MW'),zaxis = list(title = 'OB_logP')))
 fig
 
+#Visualize the result using the pca dataframe 
+df_hc <- pca_DF
+df_hc['cluster'] <- grp
+
+fig <- plot_ly(df_km, x = ~Comp.1, y = ~Comp.2, z = ~Comp.3, color = ~cluster)
+fig <- fig %>% add_markers()
+fig <- fig %>% layout(scene = list(xaxis = list(title = 'MLogP'),yaxis = list(title = 'OB_MW'),zaxis = list(title = 'OB_MR')))
+fig
 #ggplot(data = df_hc, mapping = aes(x = MLogP, y = OB_MW,color = cluster))+geom_point() 
 
 ########################################
@@ -114,61 +136,21 @@ df <- DF[,cnames[!cnames %in% c("X","id","X.1")]]
 #Perform the clustering
 #BO: eps = 2, minPts = 10
 #df_filter (small): eps = 5, minPts = 5
-#df_filter (small): eps = 6, minPts = 5
+#df_filter (small, aromatic): eps = 6, minPts = 5
 dbscan_result <- dbscan(df,eps = 2, minPts = 10)
 
 #Add clustering results to a dataframe
-df_dbscan <- Df_scaled
+df_dbscan <- df_original
 df_dbscan['cluster'] <- dbscan_result$cluster
 
 #Visualize the clustering
 df_clusters <- filter(df_dbscan, cluster > 0)
 ggplot(data = df_clusters, mapping = aes(x = MLogP,y = OB_MW, color = cluster))+geom_point() 
 
-#Function to visualize molecules for each cluster
-visualize_cluster_mols <- function(df_dbscan, cnames, nclusters){
-  for (i in 1:nclusters){
-    cluster1 <- filter(df_dbscan, cluster == i)
-    c1_id <- cluster1[cnames[cnames %in% c("id")]]
-    c1_smiles <- filter(smiles, id %in% as.list(c1_id)$id)
-    c1_sdf <- c()
-    for (i in as.list(c1_smiles['smile'])$smile){
-      c1_sdf <- c(c1_sdf, smiles2sdf(i))
-    }
-    par(mfrow=c(4,4))
-    for (i in 1:length(c1_sdf)){
-      openBabelPlot(c1_sdf[[i]])
-    }
-  }
-}
-visualize_cluster_mols(df_dbscan, cnames,5)
+fig <- plot_ly(df_clusters, x = ~MLogP, y = ~OB_MW, z = ~OB_logP, color = ~cluster)
+fig <- fig %>% add_markers()
+fig <- fig %>% layout(scene = list(xaxis = list(title = 'MLogP'),yaxis = list(title = 'OB_MW'),zaxis = list(title = 'OB_logP')))
+fig
 
-#Function to get the similarity comparison of molecules on the same cluster
-mols_similarity <- function(df_dbscan, cnames, nclusters){
-  cluster1 <- filter(df_dbscan, cluster == 1)
-  c1_id <- cluster1[cnames[cnames %in% c("id")]]
-  c1_smiles <- filter(smiles, id %in% as.list(c1_id)$id)
-  mol_similarity <- data.frame(mol1 = c1_smiles['smile']$smile[1], mol2 = c1_smiles['smile']$smile[1], Tanimoto_Coef = calcDrugMCSSim(c1_smiles['smile']$smile[1], c1_smiles['smile']$smile[1], type = 'smile')[[2]], Overlap_Coef = calcDrugMCSSim(c1_smiles['smile']$smile[1], c1_smiles['smile']$smile[1], type = 'smile')[[3]], cluster = 1)
-  for (k in 1:nclusters){  
-    cluster1 <- filter(df_dbscan, cluster == k)
-    c1_id <- cluster1[cnames[cnames %in% c("id")]]
-    c1_smiles <- filter(smiles, id %in% as.list(c1_id)$id)
-    for (i in c1_smiles['smile']$smile){
-      c_filter <- filter(c1_smiles, smile != i)
-      for (j in c_filter['smile']$smile){
-        sim <- calcDrugMCSSim(i, j, type = 'smile')
-        mol_similarity[nrow(mol_similarity) + 1,] <- data.frame(mol1 = i, mol2 = j, Tanimoto_Coef = sim[[2]], Overlap_Coef = sim[[3]], cluster = k)
-      }
-    }
-  }
-  mol_similarity <- mol_similarity[-1,]
-  return(mol_similarity)
-}
 
-df_mols_similarity <- mols_similarity(df_dbscan, cnames, 5)
 
-for (i in 1:5){
-  cluster <- filter(df_mols_similarity, cluster == i)
-  cat('\nTanimoto coef mean cluster',i,':',sum(cluster['Tanimoto_Coef'])/nrow(cluster))
-  cat('\nOverlap coef mean cluster',i,':',sum(cluster['Overlap_Coef'])/nrow(cluster))
-}
